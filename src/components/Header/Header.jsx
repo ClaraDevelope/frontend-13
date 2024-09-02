1dbb2d66f4c8a8e035d5939a3f1f38d267dee18f
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-09-02 13:51:32 +0200

    UPDATE NOTIFICATIONS AND HEADER. MAKE COMPOMENTS TO HEADER

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,69 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import moment from 'moment';
-import 'moment/locale/es'; 
-import { Box, Flex, Menu, MenuButton, MenuList, MenuItem, IconButton, Badge, Avatar, Text, Button, useDisclosure, useBreakpointValue } from '@chakra-ui/react';
-import { BellIcon, HamburgerIcon } from '@chakra-ui/icons';
-import { useAuth } from '../../providers/AuthProvider'; 
+import React from 'react';
+import { Box, Flex, useBreakpointValue } from '@chakra-ui/react';
+import { useAuth } from '../../providers/AuthProvider';
 import TitleAndLogo from './TitleAndLogo/TitleAndLogo';
-import AuthLinks from './AuthLinks/AuthLinks';
 import InputSearchUsers from '../InputSearchUsers/InputSearchUsers';
-import { useNavigate } from 'react-router-dom';
-import useApiCall from '../../hooks/useApiCall/useApiCall';
+import NotificationMenu from './NotificationMenu/NotificationMenu';
+import UserMenu from './UserMenu/UserMenu';
 
-moment.locale('es'); 
 
 const Header = () => {
-  const { isAuthenticated, logout, user } = useAuth();
-  const { isOpen: isMenuOpen, onOpen: onMenuOpen, onClose: onMenuClose } = useDisclosure();
-  const { isOpen: isNotificationOpen, onOpen: onNotificationOpen, onClose: onNotificationClose } = useDisclosure();
-  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
   const isSmallScreen = useBreakpointValue({ base: true, md: false });
-  const [notifications, setNotifications] = useState([]);
-  const [unreadCount, setUnreadCount] = useState(0);
-  const callApi = useApiCall();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      fetchNotifications();
-    }
-  }, [isAuthenticated]);
-
-  const fetchNotifications = async () => {
-    try {
-      const data = await callApi({
-        method: 'GET',
-        endpoint: '/notifications/',
-        token: user?.token,
-      });
-      setNotifications(data);
-      setUnreadCount(data.filter(n => n.status === 'pending').length);
-    } catch (error) {
-      console.error('Error al obtener notificaciones:', error);
-    }
-  };
-
-  const handleNotificationResponse = async (notificationId, response) => {
-    try {
-      await callApi({
-        method: 'POST',
-        endpoint: `/notifications/respond/${notificationId}`,
-        body: { response },
-        token: user?.token,
-      });
-      fetchNotifications();
-    } catch (error) {
-      console.error('Error al responder a la notificación:', error);
-    }
-  };
-
-  const handleLogout = () => {
-    logout();
-    navigate('/Home');
-  };
-
-  const handleMenuItemClick = () => {
-    onMenuClose();
-  };
 
   return (
     <Box
@@ -71,7 +17,7 @@ const Header = () => {
       position="absolute"
       zIndex="999"
       width="100%"
-      padding="30px"
+      padding="20px"
       display="flex"
       justifyContent="space-between"
       alignItems="center"
@@ -102,112 +48,9 @@ const Header = () => {
         )}
         
         {isAuthenticated && (
-          <Flex alignItems="center">
-            <Menu isOpen={isNotificationOpen} onOpen={onNotificationOpen} onClose={onNotificationClose}>
-              <MenuButton
-                as={IconButton}
-                icon={<BellIcon />}
-                aria-label="Notificaciones"
-                color="blue.700"
-                bg="transparent"
-                sx={{
-                  _hover: {
-                    bg: 'blue.800',
-                    color: 'white',
-                  },
-                  _active: {
-                    bg: 'blue.800',
-                    color: 'white',
-                  },
-                }}
-                position="relative"
-              >
-                {unreadCount > 0 && (
-                  <Badge
-                    colorScheme="red"
-                    position="absolute"
-                    top="-1"
-                    right="-1"
-                    borderRadius="full"
-                  >
-                    {unreadCount}
-                  </Badge>
-                )}
-              </MenuButton>
-              <MenuList>
-                {notifications.length > 0 ? (
-                  notifications.map(notification => (
-                    <Box key={notification._id}>
-                      <MenuItem
-                        bg={notification.status === 'pending' ? 'gray.100' : 'white'}
-                        _hover={{ bg: 'gray.200' }}
-                        display="flex"
-                        flexDirection="column"
-                        alignItems="center"
-                        p={3}
-                      >
-                        <Flex width="full" alignItems="center">
-                          <Avatar
-                            size="sm"
-                            src={notification.sender.profile.img}
-                            mr={3}
-                          />
-                          <Box flex="1">
-                            <Text fontWeight="bold">
-                              {`Solicitud de contacto de ${notification.sender.profile.name}`}
-                            </Text>
-                            <Text fontSize="sm" color="gray.600">
-                              {moment(notification.createdAt).fromNow()}
-                            </Text>
-                          </Box>
-                          <Flex ml={2}>
-                            <Button
-                              colorScheme="green"
-                              size="sm"
-                              onClick={() => handleNotificationResponse(notification._id, 'accepted')}
-                              mr={2}
-                            >
-                              Aceptar
-                            </Button>
-                            <Button
-                              colorScheme="red"
-                              size="sm"
-                              onClick={() => handleNotificationResponse(notification._id, 'rejected')}
-                            >
-                              Rechazar
-                            </Button>
-                          </Flex>
-                        </Flex>
-                      </MenuItem>
-                    </Box>
-                  ))
-                ) : (
-                  <MenuItem>No tienes notificaciones</MenuItem>
-                )}
-              </MenuList>
-            </Menu>
-
-            <Menu isOpen={isMenuOpen} onOpen={onMenuOpen} onClose={onMenuClose}>
-              <MenuButton
-                as={Button}
-                color="blue.700"
-                variant="ghost"
-                rightIcon={<HamburgerIcon />}
-                sx={{
-                  _hover: {
-                    bg: 'blue.800',
-                    color: 'white',
-                  },
-                  _active: {
-                    bg: 'blue.800',
-                    color: 'white',
-                  },
-                }}
-              >
-                {user.profile.name}
-              </MenuButton>
-              <AuthLinks handleLogout={handleLogout} onMenuItemClick={handleMenuItemClick} />
-            </Menu>
+          <Flex alignItems="center" position="relative">
+            <UserMenu />
+            <NotificationMenu />
           </Flex>
         )}
       </Flex>
@@ -220,7 +63,7 @@ const Header = () => {
           alignItems="stretch"
           mt="10px"
         >
-          <InputSearchUsers user={user} />
+          <InputSearchUsers />
         </Box>
       )}
     </Box>
@@ -228,3 +71,255 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import moment from 'moment';
+// import 'moment/locale/es'; 
+// import { Box, Flex, Menu, MenuButton, MenuList, MenuItem, Badge, Avatar, Text, Button, useDisclosure, useBreakpointValue } from '@chakra-ui/react';
+// import { BellIcon, HamburgerIcon } from '@chakra-ui/icons';
+// import { useAuth } from '../../providers/AuthProvider'; 
+// import TitleAndLogo from './TitleAndLogo/TitleAndLogo';
+// import AuthLinks from './AuthLinks/AuthLinks';
+// import InputSearchUsers from '../InputSearchUsers/InputSearchUsers';
+// import { useNavigate } from 'react-router-dom';
+// import useApiCall from '../../hooks/useApiCall/useApiCall';
+
+// moment.locale('es'); 
+
+// const Header = () => {
+//   const { isAuthenticated, logout, user } = useAuth();
+//   const { isOpen: isMenuOpen, onOpen: onMenuOpen, onClose: onMenuClose } = useDisclosure();
+//   const { isOpen: isNotificationOpen, onOpen: onNotificationOpen, onClose: onNotificationClose } = useDisclosure();
+//   const navigate = useNavigate();
+//   const isSmallScreen = useBreakpointValue({ base: true, md: false });
+//   const [notifications, setNotifications] = useState([]);
+//   const [unreadCount, setUnreadCount] = useState(0);
+//   const callApi = useApiCall();
+//   const token = user?.token;
+
+//   useEffect(() => {
+//     if (isAuthenticated) {
+//       fetchNotifications();
+//     }
+//   }, [isAuthenticated]);
+
+//   const fetchNotifications = async () => {
+//     try {
+//       const data = await callApi({
+//         method: 'GET',
+//         endpoint: '/notifications/',
+//         token
+//       });
+//       setNotifications(data);
+//       setUnreadCount(data.filter(n => n.status === 'pending').length);
+//     } catch (error) {
+//       console.error('Error al obtener notificaciones:', error);
+//     }
+//   };
+
+//   const handleNotificationResponse = async (notificationId, response) => {
+//     try {
+//       await callApi({
+//         method: 'POST',
+//         endpoint: `/notifications/respond/${notificationId}`,
+//         body: { response },
+//         token: user?.token,
+//       });
+//       fetchNotifications();
+//     } catch (error) {
+//       console.error('Error al responder a la notificación:', error);
+//     }
+//   };
+
+//   const handleLogout = () => {
+//     logout();
+//     navigate('/Home');
+//   };
+
+//   const handleMenuItemClick = () => {
+//     onMenuClose();
+//   };
+
+//   return (
+//     <Box
+//       as="header"
+//       position="absolute"
+//       zIndex="999"
+//       width="100%"
+//       padding="30px"
+//       display="flex"
+//       justifyContent="space-between"
+//       alignItems="center"
+//       flexWrap="wrap"
+//     >
+//       <Flex
+//         direction="row"
+//         alignItems="center"
+//         position="relative"
+//         width="full"
+//         maxW="container.xl"
+//         justifyContent="space-between"
+//       >
+//         <Flex
+//           direction="row"
+//           alignItems="center"
+//           position="relative"
+//           mr={isSmallScreen ? '0' : '30px'}
+//           flex="1"
+//         >
+//           <TitleAndLogo to={isAuthenticated ? '/Principal' : '/Home'} />
+//         </Flex>
+        
+//         {!isSmallScreen && isAuthenticated && (
+//           <Box flex="1" mx="20px">
+//             <InputSearchUsers />
+//           </Box>
+//         )}
+        
+//         {isAuthenticated && (
+//           <Flex alignItems="center" position="relative">
+//             <Box
+//               as="button"
+//               aria-label="Notificaciones"
+//               color="blue.700"
+//               bg="transparent"
+//               position="relative"
+//               borderRadius="50%"
+//               padding="5px"
+//               sx={{
+//                 _hover: {
+//                   bg: 'blue.800',
+//                   color: 'white',
+//                 },
+//                 _active: {
+//                   bg: 'blue.800',
+//                   color: 'white',
+//                 },
+//               }}
+//               onClick={onNotificationOpen}
+//             >
+//               <BellIcon boxSize={6} />
+//               {unreadCount > 0 && (
+//                 <Badge
+//                   colorScheme="red"
+//                   position="absolute"
+//                   top="20px"
+//                   right="20px"
+//                   borderRadius="50%"
+//                   zIndex="9999"
+//                   fontSize="0.7em"
+//                   padding="1px 6px"
+//                   backgroundColor="red.500"
+//                   color="white"
+//                 >
+//                   {unreadCount}
+//                 </Badge>
+//               )}
+//             </Box>
+//             <Menu isOpen={isNotificationOpen} onClose={onNotificationClose}>
+//               <MenuList position="absolute"
+//               top="10"
+//               right="-100"
+//               >
+//                 {notifications.length > 0 ? (
+//                   notifications.map(notification => (
+//                     <Box key={notification._id}>
+//                       <MenuItem
+//                         bg={notification.status === 'pending' ? 'gray.100' : 'white'}
+//                         _hover={{ bg: 'gray.200' }}
+//                         display="flex"
+//                         flexDirection="column"
+//                         alignItems="center"
+//                         p={3}
+//                       >
+//                         <Flex 
+//                         width="full" 
+//                         alignItems="center"
+//                         direction="column"
+//                         >
+//                           <Avatar
+//                             size="sm"
+//                             src={notification.sender.profile.img}
+//                             mr={3}
+//                           />
+//                           <Box flex="1">
+//                             <Text fontWeight="bold">
+//                               {`Solicitud de contacto de ${notification.sender.profile.name}`}
+//                             </Text>
+//                             <Text fontSize="sm" color="gray.600" ml="60px" >
+//                               {moment(notification.createdAt).fromNow()}
+//                             </Text>
+//                           </Box>
+//                           <Flex mt={2}>
+//                             <Button
+//                               colorScheme="green"
+//                               size="sm"
+//                               onClick={() => handleNotificationResponse(notification._id, 'accepted')}
+//                               mr={2}
+//                             >
+//                               Aceptar
+//                             </Button>
+//                             <Button
+//                               colorScheme="red"
+//                               size="sm"
+//                               onClick={() => handleNotificationResponse(notification._id, 'rejected')}
+//                             >
+//                               Rechazar
+//                             </Button>
+//                           </Flex>
+//                         </Flex>
+//                       </MenuItem>
+//                     </Box>
+//                   ))
+//                 ) : (
+//                   <MenuItem>No tienes notificaciones</MenuItem>
+//                 )}
+//               </MenuList>
+//             </Menu>
+
+//             <Menu isOpen={isMenuOpen} onOpen={onMenuOpen} onClose={onMenuClose}>
+//               <MenuButton
+//                 as={Button}
+//                 color="blue.700"
+//                 variant="ghost"
+//                 rightIcon={<HamburgerIcon />}
+//                 sx={{
+//                   _hover: {
+//                     bg: 'blue.800',
+//                     color: 'white',
+//                   },
+//                   _active: {
+//                     bg: 'blue.800',
+//                     color: 'white',
+//                   },
+//                 }}
+//               >
+//                 {user.profile.name}
+//               </MenuButton>
+//               <AuthLinks handleLogout={handleLogout} onMenuItemClick={handleMenuItemClick} />
+//             </Menu>
+//           </Flex>
+//         )}
+//       </Flex>
+      
+//       {isSmallScreen && isAuthenticated && (
+//         <Box
+//           width="full"
+//           display="flex"
+//           flexDirection="column"
+//           alignItems="stretch"
+//           mt="10px"
+//         >
+//           <InputSearchUsers user={user} />
+//         </Box>
+//       )}
+//     </Box>
+//   );
+// };
+
+// export default Header;
+
+
---
+++ src/components/Header/NotificationMenu/NotificationMenu.jsx
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import 'moment/locale/es'; 
+import {
+  Box, Badge, Menu, MenuList, MenuItem, Flex, Avatar, Text, Button, useDisclosure
+} from '@chakra-ui/react';
+import { BellIcon } from '@chakra-ui/icons';
+import useApiCall from '../../../hooks/useApiCall/useApiCall';
+import { useAuth } from '../../../providers/AuthProvider';
+
+moment.locale('es');
+
+const NotificationMenu = () => {
+  const { user } = useAuth();
+  const callApi = useApiCall();
+  const token = user?.token;
+  const [notifications, setNotifications] = useState([]);
+  const [unreadCount, setUnreadCount] = useState(0);
+  const { isOpen: isNotificationOpen, onOpen: onNotificationOpen, onClose: onNotificationClose } = useDisclosure();
+
+  useEffect(() => {
+    if (user) {
+      fetchNotifications();
+    }
+  }, [user]);
+
+  const fetchNotifications = async () => {
+    try {
+      const data = await callApi({
+        method: 'GET',
+        endpoint: '/notifications/',
+        token
+      });
+      setNotifications(data);
+      setUnreadCount(data.filter(n => n.status === 'pending').length);
+    } catch (error) {
+      console.error('Error al obtener notificaciones:', error);
+    }
+  };
+
+  const handleNotificationResponse = async (notificationId, response) => {
+    try {
+      await callApi({
+        method: 'POST',
+        endpoint: `/notifications/respond/${notificationId}`,
+        body: { response },
+        token: user?.token,
+      });
+      fetchNotifications();
+    } catch (error) {
+      console.error('Error al responder a la notificación:', error);
+    }
+  };
+
+  return (
+    <Box position="relative">
+      <Menu isOpen={isNotificationOpen} onClose={onNotificationClose}>
+        <Button
+          role="button"
+          aria-label="Notificaciones"
+          color= 'blue.700'
+            backgroundColor= 'transparent'
+            borderRadius= '50%'
+            padding= '10px 2px'
+            cursor= 'pointer'
+            display= 'flex'
+            alignItems= 'center'
+            position= 'relative'
+            sx={{
+                _hover: 
+                {
+                bg: 'blue.800',
+                color: 'white',
+                },
+                _active: {
+                bg: 'blue.800',
+                color: 'white',
+                },
+              }}
+          onClick={onNotificationOpen}
+        >
+          <BellIcon boxSize={6}/>
+          {unreadCount > 0 && (
+            <Badge
+              colorScheme="red"
+              position="absolute"
+              top="25px"
+              right="25px"
+              borderRadius="50%"
+              zIndex="9999"
+              fontSize="0.7em"
+              padding="5px 8px 3px 8px"
+              backgroundColor="red.500"
+              color="white"
+            >
+              {unreadCount}
+            </Badge>
+          )}
+        </Button>
+        <MenuList
+          position="absolute"
+          top="40px"
+          right="-40px"
+          minWidth="300px"
+        >
+          {notifications.length > 0 ? (
+            notifications.map(notification => (
+              <MenuItem
+                key={notification._id}
+                bg={notification.status === 'pending' ? 'gray.100' : 'white'}
+                _hover={{ bg: 'gray.200' }}
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                p={3}
+              >
+                <Flex width="full" alignItems="center" direction="column">
+                  <Avatar
+                    size="sm"
+                    src={notification.sender.profile.img}
+                    mr={3}
+                  />
+                  <Box flex="1">
+                    <Text fontWeight="bold">
+                      {`Solicitud de contacto de ${notification.sender.profile.name}`}
+                    </Text>
+                    <Text fontSize="sm" color="gray.600">
+                      {moment(notification.createdAt).fromNow()}
+                    </Text>
+                  </Box>
+                  <Flex mt={2}>
+                    <Button
+                      colorScheme="green"
+                      size="sm"
+                      onClick={() => handleNotificationResponse(notification._id, 'accepted')}
+                      mr={2}
+                    >
+                      Aceptar
+                    </Button>
+                    <Button
+                      colorScheme="red"
+                      size="sm"
+                      onClick={() => handleNotificationResponse(notification._id, 'rejected')}
+                    >
+                      Rechazar
+                    </Button>
+                  </Flex>
+                </Flex>
+              </MenuItem>
+            ))
+          ) : (
+            <MenuItem>No tienes notificaciones</MenuItem>
+          )}
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
+
+export default NotificationMenu;
+
